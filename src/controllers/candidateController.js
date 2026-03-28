b5ec8b35936cee6f77ad61014bb74bc393ac89d9
Zibnu <crimhomid@gmail.com>
2026-03-28 10:09:27 +0700

    create update,delete and get all candidate for candidate controller

--- src/controllers/candidateController.js
+++ src/controllers/candidateController.js
@@ -2,6 +2,7 @@ const { Candidate } = require("../models");
 const fs = require("fs");
 const path = require("path");
 
+// ✨✨
 exports.createCandidate = async (req, res) => {
     try {
         const { ketua_name, wakil_name, visi, misi} = req.body;
@@ -43,6 +44,113 @@ exports.createCandidate = async (req, res) => {
         return res.status(500).json({
             success : false,
             message : "Internal Server Error",
+            error : error.message,
+        });
+    }
+};
+// ✨✨
+exports.getAllCandidate = async (req, res) => {
+    try {
+        const candidate = await Candidate.findAll({
+            attributes : ["id_candidate", "ketua_name", "wakil_name", "visi", "misi", "image"],
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Get All Data Candidate Success",
+            data : candidate,
+        });
+    } catch (error) {
+        console.error("Get All Candidate ERROR", error);
+        return res.status(500).json({
+            success : false,
+            message : "Internal Server ERROR",
+            error : error.message,
+        });
+    }
+};
+// ✨✨
+exports.updateCandidate = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { ketua_name, wakil_name, visi, misi } = req.body;
+
+        const candidate = await Candidate.findByPk(id);
+
+        if(!candidate) {
+            return res.status(404).json({
+                success : false,
+                message : "Candidate Not Found",
+            });
+        }
+
+        let imageUrl = candidate.image;
+
+        if(req.file) {
+            const oldImagePath = path.join("uploads/image", path.basename(candidate.image));
+
+            // delete old image in root project to new image
+            if(fs.existsSync(oldImagePath)) {
+                fs.unlinkSync(oldImagePath);
+            }
+
+            imageUrl = `${req.protocol}://${req.get("host")}/uploads/image/${req.file.filename}`;
+        }
+
+        await candidate.update({
+            ketua_name,
+            wakil_name,
+            visi,
+            misi,
+            image : imageUrl,
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Update Data Candidate Success",
+            data : candidate,
+        });
+    } catch (error) {
+        console.error("Update Candidate ERROR", error);
+        return res.status(500).json({
+            success : false,
+            message : "Internal Server ERROR",
+            error : error.message,
+        });
+    }
+}
+
+// ✨✨
+exports.deleteCandidate = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const candidate = await Candidate.findByPk(id);
+
+        if(!candidate) {
+            return res.status(404).json({
+                success : false,
+                message : "Candidate Not Found",
+            });
+        }
+
+        const imagePath = path.join("uploads/image", path.basename(candidate.image));
+
+        if(fs.existsSync(imagePath)) {
+            fs.unlinkSync(imagePath);
+        };
+
+        await candidate.destroy();
+
+        return res.status(200).json({
+            success : true,
+            message : "Delete Candidate Success",
+        });
+    } catch (error) {
+        console.error("Delete Candidate ERROR", error);
+        return res.status(500).json({
+            success : false,
+            message : "Internal Server ERROR",
             error : error.message,
         });
     }
--- src/routes/candidateRouter.js
+++ src/routes/candidateRouter.js
@@ -5,6 +5,9 @@ const { authenticate, isAdmin} = require("../middleware/authMiddleware");
 const uploadImage = require("../middleware/uploadImage");
 
 router.post("/create_candidate", authenticate, isAdmin, uploadImage.single("image"), candidateController.createCandidate)
+router.get("/candidates", authenticate, candidateController.getAllCandidate);
+router.patch("/candidate/:id", authenticate, isAdmin, uploadImage.single("image"), candidateController.updateCandidate);
+router.delete("/candidate/:id", authenticate, isAdmin, candidateController.deleteCandidate);
 
 
 module.exports = router;
