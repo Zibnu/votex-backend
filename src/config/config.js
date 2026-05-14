1c347977d870f6517a5e886a99570ebbfa65b9ba
Zibnu <crimhomid@gmail.com>
2026-05-14 22:26:21 +0700

    update config.js and db.js for deploy in railway

--- src/config/config.js
+++ src/config/config.js
@@ -1,45 +1,48 @@
 require("dotenv").config();
 
 module.exports = {
-    development : {
-        username : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
-        host : process.env.DB_HOST,
-        dialect : process.env.DB_DIALECT,
-        miggrationStorageTableName : "migrations",
-        migrationStorage : "sequelize",
-        migrations : {
-            path : "src/migrations",
-            pattern : /\.js$/,
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        miggrationStorageTableName: "migrations",
+        migrationStorage: "sequelize",
+        migrations: {
+            path: "src/migrations",
+            pattern: /\.js$/,
         },
-        seederStorage : "sequelize",
-        seederTableName : "seeders",
+        seederStorage: "sequelize",
+        seederTableName: "seeders",
     },
-    test : {
-        username : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
-        host : process.env.DB_HOST,
-        dialect : process.env.DB_DIALECT,
-        miggrationStorageTableName : "migrations",
-        migrationStorage : "sequelize",
-        migrations : {
-            path : "src/migrations",
-            pattern : /\.js$/,
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        miggrationStorageTableName: "migrations",
+        migrationStorage: "sequelize",
+        migrations: {
+            path: "src/migrations",
+            pattern: /\.js$/,
         },
     },
-    production : {
-        username : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
-        host : process.env.DB_HOST,
-        dialect : process.env.DB_DIALECT,
-        miggrationStorageTableName : "migrations",
-        migrationStorage : "sequelize",
-        migrations : {
-            path : "src/migrations",
-            pattern : /\.js$/,
+    production: {
+        use_env_variable: "DB_URL",
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        migrationStorageTableName: "migrations",
+        migrationStorage: "sequelize",
+        migrations: {
+            path: "src/migrations",
+            pattern: /\.js$/,
         },
     },
 };
--- src/config/db.js
+++ src/config/db.js
@@ -6,10 +6,16 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,
-    dialect : process.env.DB_DIALECT,
-    logging : process.env.DB_LOGGING === false,// for not spam query in console
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: process.env.DB_LOGGING === "true",
+    dialectOptions: process.env.NODE_ENV === "production" ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    } : {},
   }
 );
 
